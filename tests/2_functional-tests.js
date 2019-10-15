2e4aca393564178c5a6c964af693098e97a3270e
Jared Su <jaredsu1989@gmail.com>
2019-10-15 12:01:40 +1100

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,6 +9,7 @@
 var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
+var expect = chai.expect;
 var server = require('../server');
 
 chai.use(chaiHttp);
@@ -41,11 +42,33 @@ suite('Functional Tests', function() {
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+        chai.request(server)
+        .post('/api/books')
+        .send({
+          title: 'title'
+        })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.title, 'title');
+          assert.exists(res.body.title);
+          expect(res.body).to.be.a('object');
+          done();
+        });
+        
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+        chai.request(server)
+        .post('/api/books')
+        .send({
+          
+        })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          //assert.equal(res.body.title, 'title');
+          assert.equal(res.body.title, undefined);
+          done();
+        });
       });
       
     });
@@ -54,7 +77,17 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai.request(server)
+      .get('/api/books')
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'response should be an array');
+        assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+        assert.property(res.body[0], 'title', 'Books in array should contain title');
+        assert.property(res.body[0], '_id', 'Books in array should contain _id');
+        assert.isNumber(res.body[0].commentcount, 'commentcount value should be a number')
+        done();
+      });
       });      
       
     });
@@ -62,12 +95,27 @@ suite('Functional Tests', function() {
 
     suite('GET /api/books/[id] => book object with [id]', function(){
       
-      test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+      test('Test GET /api/books/:id with id not in db',  function(done){
+        chai.request(server)
+      .get('/api/books/:id')
+      .end(function(err, res){        
+        assert.equal(res.status, 200);        
+        assert.equal(res.text, 'no book exists');       
+        done();
+      });
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai.request(server)
+      .get('/api/books/5d633ece432b5904ff412a92')
+      .end(function(err, res){  
+          
+        assert.equal(res.status, 200);        
+        assert.isArray(res.body.comments, 'response should be an array');        
+        assert.property(res.body, 'title');
+        assert.property(res.body, '_id');       
+        done();
+      });
       });
       
     });
@@ -76,7 +124,23 @@ suite('Functional Tests', function() {
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        chai.request(server)
+        .post('/api/books/5d633ece432b5904ff412a92')
+        .send({
+          _id: '5d633ece432b5904ff412a92',
+          comment: 'This is a test comment'
+        })
+        .end(function(err, res) {
+          console.log(res.text);
+          assert.equal(res.status, 200);
+          //assert.equal(res.body.title, 'title');
+          assert.property(res.body, 'title');
+          assert.property(res.body, '_id');
+          assert.isArray(res.body.comments, 'response should be an array');
+          expect(res.body).to.have.property('_id');
+          done();
+          
+      });
       });
       
     });
